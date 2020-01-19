39d5df89408f2786226a2b5bc972dff9bef4401b
BelgianNoise <king.arthur360@gmail.com>
2020-01-19 19:42:20 +0100

    status update kendem

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const requestList = document.querySelector('#request-list');
 const form = document.querySelector('#add-request-form');
 
 // create element & render cafe
-function renderCafe(doc){
+function renderRequest(doc){
     // li
     let li = document.createElement('li');
     li.setAttribute('data-id', doc.id);
@@ -37,7 +37,6 @@ function renderCafe(doc){
     li.style.backgroundRepeat = 'no-repeat';
     li.style.backgroundPosition = 'right';
     li.style.backgroundSize = '60%';
-    li.style.backgroundColor
     switch (doc.data().status){
         case '0': // not started
             li.style.backgroundImage = 'url(\'img/pending_trans.jpg\')';
@@ -69,6 +68,44 @@ function renderCafe(doc){
         db.collection('requests').doc(id).delete();
     });
 
+    // update
+    let update = document.createElement('div');
+    update.className = 'update';
+    update.textContent = 'Verander status';
+    // drop down
+    let drop = document.createElement('div');
+    drop.className = 'drop';
+    let btn1 = document.createElement('button');
+    btn1.textContent = 'Finished';
+    btn1.addEventListener('click',(e)=>{
+        let id = e.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+        db.collection('requests').doc(id).update({status: '1'});
+    });
+    drop.appendChild(btn1);
+    let btn2 = document.createElement('button');
+    btn2.textContent = 'Pending...';
+    btn2.addEventListener('click',(e)=>{
+        let id = e.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+        db.collection('requests').doc(id).update({status: '0'});
+    });
+    drop.appendChild(btn2);
+    let btn3 = document.createElement('button');
+    btn3.textContent = 'No Can Do';
+    btn3.addEventListener('click',(e)=>{
+        let id = e.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+        db.collection('requests').doc(id).update({status: '-'});
+    });
+    drop.appendChild(btn3);
+    let btn4 = document.createElement('button');
+    btn4.textContent = 'In Progress...';
+    btn4.addEventListener('click',(e)=>{
+        let id = e.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+        db.collection('requests').doc(id).update({status: 'b'});
+    });
+    drop.appendChild(btn4);
+    update.appendChild(drop);
+
+    li.appendChild(update);
     // add to list
     requestList.prepend(li);
 }
@@ -111,13 +148,38 @@ db.collection('requests').orderBy('date').onSnapshot(snapshot => {
     let changes = snapshot.docChanges();
     changes.forEach(change => {
         if(change.type == 'added'){
-            renderCafe(change.doc);
+            renderRequest(change.doc);
         } else if (change.type == 'removed'){
             let li = requestList.querySelector('[data-id=' + change.doc.id + ']');
             requestList.removeChild(li);
+        } else if (change.type == 'modified'){
+            updateBackground(change.doc)
         }
     });
 });
+
+function updateBackground(doc){
+    let li = requestList.querySelector('[data-id=' + doc.id + ']');
+    switch (doc.data().status){
+        case '0': // not started
+            li.style.backgroundImage = 'url(\'img/pending_trans.jpg\')';
+            li.style.backgroundColor = 'rgba(0,84,165,0.4)'
+            break;
+        case '1': // finished
+            li.style.backgroundImage = 'url(\'img/finished_trans.jpg\')';
+            li.style.backgroundColor = 'rgba(0,166,84,0.4)'
+            break;
+        case '-': // no can do
+            li.style.backgroundImage = 'url(\'img/no_can_do_trans.jpg\')';
+            li.style.backgroundColor = 'rgba(237,27,36,0.4)'
+            break;
+        case 'b': // bezig
+            li.style.backgroundImage = 'url(\'img/busy_trans.jpg\')';
+            li.style.backgroundColor = 'rgba(254,242,0,0.4)'
+            break;
+        default:
+    }
+}
 
 // pop up voor legende
 document.querySelector('#legende').addEventListener('click',
